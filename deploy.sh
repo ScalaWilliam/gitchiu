657f3308fad0e23a217e7776cc5c30828ab39976
Vykintas Narmontas (William) <william@scalawilliam.com>
2017-04-07 20:51:44 +0800

    Spelling error.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 touch lambda.zip
 aws s3api put-object --bucket gitshu --key fn/gitshu-$(git describe --tags).zip --body lambda.zip --acl public-read
 aws s3api put-object --bucket gitshu --key fn/gitshu-latest.zip --body lambda.zip --acl public-read
-aws s3api put-object --bucket gitshy --key cloudformation.yml --body cloudformation.yml --acl public-read
+aws s3api put-object --bucket gitshu --key cloudformation.yml --body cloudformation.yml --acl public-read
 
